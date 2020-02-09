67d9cd21dc57483f64e7d53b6250f29e230314ef
Readme0 <1130203417@qq.com>
2020-02-09 23:11:38 +0800

    学习java集合与泛型

--- app/src/main/java/com/example/gimalaya/MainActivity.java
+++ app/src/main/java/com/example/gimalaya/MainActivity.java
@@ -20,13 +20,13 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Map<String, String> map = new HashMap<>();
+    protected void onCreate(Bundle savedInstanceState) {  //子类重写了父类的onCreate方法，在活动启动时该方法会被调用
+        super.onCreate(savedInstanceState);  //调用父类的onCreate方法
+        setContentView(R.layout.activity_main);   //由于是继承自父类，因此子类也继承了父类的setContentView方法，这里是调用自己的setContentView方法
+        Map<String, String> map = new HashMap<>();  //集合map(映射)，key与value一一对应，且key不可以重复，通过key访问value
         CommonRequest.getCategories(map, new IDataCallBack<CategoryList>() {
             @Override
-            public void onSuccess(CategoryList categoryList) {
+            public void onSuccess(CategoryList categoryList) {  //这个函数先这么理解:传入了一个数据结构map，最后回调到onSuccess函数
                 List<Category> categories = categoryList.getCategories();
                 if (categories != null) {
                     int size = categories.size();
